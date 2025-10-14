dcc25fc3de4699ba7ad2e6f67fee1fd4746a9173
Aditya-Shukla4 <as1384909@gmail.com>
2025-10-15 01:54:01 +0530

    next config fix for deployment

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,14 +1,14 @@
-// next.config.mjs
-import transpileModules from "next-transpile-modules";
-
-const withTM = transpileModules(["monaco-editor"]);
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
     return config;
   },
+
+  // This is the new, built-in way to transpile packages.
+  // The old 'next-transpile-modules' is no longer needed.
+  transpilePackages: ["monaco-editor"],
 };
 
-export default withTM(nextConfig);
+export default nextConfig;
